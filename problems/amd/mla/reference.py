419df534a9831fe7a39dfbbc107802522dca5e84
danielhua23 <daniel.huang2@amd.com>
2025-05-13 05:51:16 +0000

    add simplified rope and linear into MLA kernel

--- problems/amd/mla/reference.py
+++ problems/amd/mla/reference.py
@@ -1,75 +1,114 @@
-import torch
-import torch.nn as nn
 import math
-import random
+from dataclasses import dataclass
+import torch
+from torch import nn
+import torch.nn.functional as F
 from task import input_t, output_t
 from utils import make_match_reference
-from typing import Optional, Tuple, Union
 
-def generate_input(b, d, dv, hq, sq, hkv, meansk, seed):
+class RoPE(nn.Module):
+    def __init__(self, d_model: int):
+        super().__init__()
+        self.d_model = d_model
+        theta = 10000 ** (-torch.arange(0, d_model//2, dtype=torch.float) / (d_model//2))
+        self.register_buffer("theta", theta)
 
-    cache_seqlens = torch.full((b,), meansk, dtype=torch.int32)
-    max_seqlen = cache_seqlens.max().item()
-    max_seqlen_pad = math.ceil(max_seqlen/256) * 256
+    def rotate_half(self, x: torch.Tensor) -> torch.Tensor:
+        x1, x2 = x.chunk(2, dim=-1)
+        return torch.cat((-x2, x1), dim=-1)
 
+    def forward(self, x: torch.Tensor, start_pos: int = 0) -> torch.Tensor:
+        seq_len = x.size(-2)
+        d_model = x.size(-1)
+        assert d_model == self.d_model
+        seq_idx = torch.arange(start_pos, start_pos + seq_len, device=x.device)
+        idx_theta = torch.einsum('s,d->sd', seq_idx, self.theta)
+        idx_theta2 = torch.cat([idx_theta, idx_theta], dim=-1)
+        cos = idx_theta2.cos()
+        sin = idx_theta2.sin()
+        return x * cos + self.rotate_half(x) * sin
+
+@dataclass
+class Config:
+    bs: int
+    sq: int
+    dim: int
+    n_heads: int
+    q_lora_rank: int 
+    kv_lora_rank: int
+    qk_nope_head_dim: int
+    qk_rope_head_dim: int
+    v_head_dim: int
+
+class MLA(nn.Module):
+    def __init__(self, config: Config):
+        super().__init__()
+        self.dim = config.dim
+        self.n_heads = config.n_heads
+        self.q_lora_rank = config.q_lora_rank
+        self.kv_lora_rank = config.kv_lora_rank
+        self.qk_nope_head_dim = config.qk_nope_head_dim
+        self.qk_rope_head_dim = config.qk_rope_head_dim
+        self.v_head_dim = config.v_head_dim
+        self.wq_a = nn.Linear(self.dim, self.q_lora_rank, dtype=torch.bfloat16, bias=False)
+        self.wq_b = nn.Linear(self.q_lora_rank, (self.qk_nope_head_dim + self.qk_rope_head_dim) * self.n_heads, dtype=torch.bfloat16, bias=False)
+        self.q_rope = RoPE(self.qk_rope_head_dim)
+        self.wkv_a = nn.Linear(self.dim, self.kv_lora_rank + self.qk_rope_head_dim, dtype=torch.bfloat16, bias=False)
+        self.wkv_b = nn.Linear(self.kv_lora_rank, (self.qk_nope_head_dim + self.v_head_dim)* self.n_heads, dtype=torch.bfloat16, bias=False)
+        self.k_rope = RoPE(self.qk_rope_head_dim)
+        self.wo = nn.Linear(self.v_head_dim * self.n_heads, self.dim, dtype=torch.bfloat16, bias=False)
+        self.eps =1e-6
+   
+    def forward(self,x: torch.Tensor) -> torch.Tensor:
+        bsz, seq_len, _ = x.size()
+        q = self.wq_a(x)
+        q = self.wq_b(q)   
+        q = q.view(bsz, seq_len, self.n_heads, self.qk_rope_head_dim + self.qk_nope_head_dim)
+        q_nope, q_rope = torch.split(q, [self.qk_nope_head_dim, self.qk_rope_head_dim], dim=-1)
+        q_rope = q_rope.view(bsz, self.n_heads, seq_len, self.qk_rope_head_dim)
+        start_pos = 0
+        q_rope = self.q_rope(q_rope, start_pos)
+        q_rope = q_rope.view(bsz, seq_len, self.n_heads, self.qk_rope_head_dim)
+        q = torch.cat([q_nope, q_rope.expand(-1,-1,self.n_heads,-1)], dim=-1)
+        
+        kv = self.wkv_a(x)
+        kv, k_pe = torch.split(kv, [self.kv_lora_rank, self.qk_rope_head_dim], dim=-1)
+        k_pe = k_pe.unsqueeze(2)
+        k_pe = self.k_rope(k_pe, start_pos)
+        kv = self.wkv_b(kv).view(bsz, seq_len, self.n_heads, self.qk_nope_head_dim + self.v_head_dim)
+        k_nope,v = torch.split(kv,[self.qk_nope_head_dim,self.v_head_dim], dim=-1)
+        k = torch.concat([k_nope, k_pe.expand(-1,-1,self.n_heads,-1)], dim=-1)
+                
+        scores = torch.matmul(q, k.transpose(-1, -2)) / math.sqrt(self.qk_rope_head_dim + self.qk_nope_head_dim)
+        attn = F.softmax(scores, dim=-1).to(torch.bfloat16)
+        y = torch.matmul(attn, v)
+        return y
+
+def generate_input(bs, sq, dim, seed):
+    config = Config(
+        bs=bs,
+        sq=sq,
+        dim=dim,
+        n_heads=128,
+        q_lora_rank=1536,
+        kv_lora_rank=512,
+        qk_nope_head_dim=128,
+        qk_rope_head_dim=64,
+        v_head_dim=128,
+    )
     gen = torch.Generator()
     gen.manual_seed(seed)
-
-    q = torch.randn((b, sq, hq, d), dtype=torch.bfloat16, generator=gen)
-    k = torch.randn((b, max_seqlen_pad, hkv, d), dtype=torch.bfloat16, generator=gen)
-    v = torch.randn((b, max_seqlen_pad, hkv, dv), dtype=torch.bfloat16, generator=gen)
-    return q, k, v, cache_seqlens, max_seqlen_pad
-
-def scaled_dot_product_attention(query, key, value, hq, hkv, is_causal=False):
-    key = key.repeat_interleave(hq // hkv, dim=0)
-    value = value.repeat_interleave(hq // hkv, dim=0)
-
-    scale = 1.0 / math.sqrt(query.size(-1))
-    attn_weight = torch.matmul(query, key.transpose(-2, -1)) * scale
-
-    # Apply causal mask if needed
-    if is_causal:
-        sq = query.shape[-2]
-        sk = key.shape[-2]
-        attn_bias = torch.zeros(sq, sk, dtype=torch.float32, device=query.device)
-        temp_mask = torch.ones(sq, sk, dtype=torch.bool, device=query.device).tril(diagonal=sk - sq)
-        attn_bias.masked_fill_(temp_mask.logical_not(), float("-inf"))
-        attn_weight += attn_bias
-
-    attn_weight = torch.nn.functional.softmax(attn_weight, dim=-1)
-    out = torch.matmul(attn_weight, value)
-
-    return out
+    x = torch.randn((bs, sq, dim), dtype=torch.bfloat16, generator=gen)
+    return config, x
 
 def ref_kernel(data: input_t) -> output_t:
-    """
-    q shape: batch_size, q_seqlen, hq, d
-    k shape: batch_size, max_seqlen_pad, hkv, d
-    v shape: batch_size, max_seqlen_pad, hkv, d_v
-    cache_seqlens: tensor containing the actual sequence lengths
-    max_seqlen_pad: the padded sequence length
-    positions: tensor containing position information for RoPE
-    """
-    q, k, v, cache_seqlens, max_seqlen_pad = data
-    causal = False
-    b, sq, hq, d = q.shape
-    _, _, hkv, dv = v.shape
-    out = torch.empty(b, sq, hq, dv, dtype=q.dtype)
-    for i in range(b):
-        begin = i * max_seqlen_pad
-        end = begin + cache_seqlens[i]
-        ik = k.view(-1, hkv, d)[begin:end]
-        iv = v.view(-1, hkv, dv)[begin:end]
-        iq = q[i]
-        O = scaled_dot_product_attention(
-                iq.transpose(0, 1),
-                ik.transpose(0, 1),
-                iv.transpose(0, 1),
-                hq=hq,
-                hkv=hkv,
-                is_causal=causal,
-            )
-        out[i] = O.transpose(0, 1)
-    return out
+    config, x= data
+    model = MLA(config)
+    output = model(x)
+    return output
 
-check_implementation = make_match_reference(ref_kernel)
+check_implementation = make_match_reference(ref_kernel, rtol=2e-02, atol=1e-03)         
+    
+    
+    
+  
